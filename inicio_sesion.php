68d10f063c17a37db4dcc68108f43d7439a24070
fdss2023 <150443687+fdss2023@users.noreply.github.com>
2023-11-10 19:00:06 -0500

    Add files via upload

---
+++ inicio_sesion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action ="sisas2.php" mathod="post" >
+    <samp>escriba su nombre</samp>
+    <br>
+    <input type="text" name ="nombre">
+    <br>
+    <samp>escriba su telefono</samp>
+    <br>
+    <input type="text" name ="telefono">
+    <input type ="submit" value ="guardar">
+ </form>
+</body>
+</html>
